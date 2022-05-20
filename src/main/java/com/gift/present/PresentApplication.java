d17a1d5acf533d2c92d6e95724cda96e9724a95a
mylhb9 <mylhb9@naver.com>
2022-05-20 18:51:23 +0900

    init

---
+++ src/main/java/com/gift/present/PresentApplication.java
@@ -0,0 +1,13 @@
+package com.gift.present;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PresentApplication.class, args);
+    }
+
+}
